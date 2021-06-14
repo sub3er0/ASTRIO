15be83bcece0f7ed6e7846dcc5475d77f639696e
sub3er0 <alexkrainov93@gmail.com>
2021-06-14 11:28:19 +0400

    init

---
+++ Astrio/TrainingModule1/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Astrio_TrainingModule1',
+    __DIR__
+);
---
+++ Astrio/TrainingModule2/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Astrio_TrainingModule2',
+    __DIR__
+);
